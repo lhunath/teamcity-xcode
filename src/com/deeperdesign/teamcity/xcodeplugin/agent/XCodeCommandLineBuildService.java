fc897f7568ce17a897e57a281ff7b4145910d289
Maarten Billemont <lhunath@gmail.com>
2011-06-23 22:15:20 +0200

    Updated for TeamCity 6.0.3

--- src/com/deeperdesign/teamcity/xcodeplugin/agent/XCodeCommandLineBuildService.java
+++ src/com/deeperdesign/teamcity/xcodeplugin/agent/XCodeCommandLineBuildService.java
@@ -11,52 +11,52 @@ import jetbrains.buildServer.agent.runner.*;
  *
  * @author oliver
  */
-public class XCodeCommandLineBuildService extends CommandLineBuildService {
-  
+public class XCodeCommandLineBuildService extends BuildServiceAdapter {
+
 
     public ProgramCommandLine makeProgramCommandLine() {
-        return new SimpleProgramCommandLine(getBuild(), getExecutablePath(), getArguments());
+        return createProgramCommandline(getExecutablePath(), getArguments());
     }
 
     protected List<String> getArguments() {
         List<String> arguments = new Vector<String>();
         final Map<String, String> params = getBuild().getRunnerParameters();
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_PROJECT)) {
             arguments.add("-project");
-            arguments.add(params.get(PluginConstants.SETTINGS_XCODE_PROJECT));        
+            arguments.add(params.get(PluginConstants.SETTINGS_XCODE_PROJECT));
         }
-       
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_TARGET)) {
             String target = params.get(PluginConstants.SETTINGS_XCODE_TARGET);
-            
+
             if(target.equals(PluginConstants.SETTINGS_XCODE_TARGET_ACTIVETARGET)) {
                 arguments.add("-activetarget");
-            }    
+            }
             else if(target.equals(PluginConstants.SETTINGS_XCODE_TARGET_ALLTARGETS)) {
                 arguments.add("-alltargets");
-            } 
+            }
             else if(target.equals(PluginConstants.SETTINGS_XCODE_TARGET_SINGLETARGET)) {
                 arguments.add("-target");
                 arguments.add(params.get(PluginConstants.SETTINGS_XCODE_TARGETNAME));
             }
         }
-        
+
         if(isParameterEnabled(params, PluginConstants.SETTINGS_XCODE_PARALLELIZETARGETS)) {
             arguments.add("-parallelizeTargets");
         }
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_CONFIGURATION)) {
             arguments.add("-configuration");
             arguments.add(params.get(PluginConstants.SETTINGS_XCODE_CONFIGURATION));
         }
         // TODO: support -activeconfiguration ?
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_SDK)) {
             arguments.add("-sdk");
             arguments.add(params.get(PluginConstants.SETTINGS_XCODE_SDK));
         }
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_BUILD_ACTION)) {
             String action = params.get(PluginConstants.SETTINGS_XCODE_BUILD_ACTION).trim();
             String[] actions = action.split(" ");
@@ -64,7 +64,7 @@ public class XCodeCommandLineBuildService extends CommandLineBuildService {
                 arguments.add(actions[i]);
             }
         }
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_BUILD_SETTINGS)) {
             for(String setting : splitSettings(params.get(PluginConstants.SETTINGS_XCODE_BUILD_SETTINGS))) {
                 if(setting != null && setting.trim().equals("") == false) {
@@ -72,7 +72,7 @@ public class XCodeCommandLineBuildService extends CommandLineBuildService {
                 }
             }
         }
-        
+
         if(parameterExists(params, PluginConstants.SETTINGS_XCODE_USER_DEFAULTS)) {
             for(String setting : splitSettings(params.get(PluginConstants.SETTINGS_XCODE_USER_DEFAULTS))) {
                 if(setting != null && setting.trim().equals("") == false) {
@@ -86,11 +86,11 @@ public class XCodeCommandLineBuildService extends CommandLineBuildService {
     protected String getExecutablePath() {
         return "xcodebuild";
     }
- 
+
     private static boolean parameterExists(final Map<String, String> params, final String key) {
-        return params.containsKey(key) && (params.get(key).trim().equals("") == false);            
+        return params.containsKey(key) && (params.get(key).trim().equals("") == false);
     }
-    
+
     private static boolean isParameterEnabled(final Map<String, String> params, final String key) {
         return params.containsKey(key) && params.get(key).equalsIgnoreCase(Boolean.TRUE.toString());
     }
@@ -104,5 +104,5 @@ public class XCodeCommandLineBuildService extends CommandLineBuildService {
             }
 
         return list;
-    }    
+    }
 }
